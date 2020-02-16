8e5487aba7850e7da26bda20f866c7731d33115c
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-17 00:20:30 +0900

    Fix tests

--- run_test.go
+++ run_test.go
@@ -24,48 +24,48 @@ func testRun(t *testing.T, input, output string) {
 func TestRun(t *testing.T) {
 	t.Run("Heroku ClearDB", func(t *testing.T) {
 		input := "mysql://USERNAME:PASSWORD@REGIONNAME.cleardb.net/heroku_92599a6734fc1a6?reconnect=true"
-		output := `DATABASE_USER=USERNAME
-DATABASE_PASSWORD=PASSWORD
-DATABASE_SCHEME=mysql
-DATABASE_HOST=REGIONNAME.cleardb.net
-DATABASE_PORT=3306
-DATABASE_DB=heroku_92599a6734fc1a6
+		output := `export DATABASE_USER=USERNAME
+export DATABASE_PASSWORD=PASSWORD
+export DATABASE_SCHEME=mysql
+export DATABASE_HOST=REGIONNAME.cleardb.net
+export DATABASE_PORT=3306
+export DATABASE_DB=heroku_92599a6734fc1a6
 `
 		testRun(t, input, output)
 	})
 
 	t.Run("Heroku Postgres", func(t *testing.T) {
 		input := "postgres://USERNAME:PASSWORD@REGION-NAME.compute-1.amazonaws.com:5432/d2eel7vtbj70hk"
-		output := `DATABASE_USER=USERNAME
-DATABASE_PASSWORD=PASSWORD
-DATABASE_SCHEME=postgres
-DATABASE_HOST=REGION-NAME.compute-1.amazonaws.com
-DATABASE_PORT=5432
-DATABASE_DB=d2eel7vtbj70hk
+		output := `export DATABASE_USER=USERNAME
+export DATABASE_PASSWORD=PASSWORD
+export DATABASE_SCHEME=postgres
+export DATABASE_HOST=REGION-NAME.compute-1.amazonaws.com
+export DATABASE_PORT=5432
+export DATABASE_DB=d2eel7vtbj70hk
 `
 		testRun(t, input, output)
 	})
 
 	t.Run("Postgres no port", func(t *testing.T) {
 		input := "postgres://USERNAME:PASSWORD@REGION-NAME.compute-1.amazonaws.com/d2eel7vtbj70hk"
-		output := `DATABASE_USER=USERNAME
-DATABASE_PASSWORD=PASSWORD
-DATABASE_SCHEME=postgres
-DATABASE_HOST=REGION-NAME.compute-1.amazonaws.com
-DATABASE_PORT=5432
-DATABASE_DB=d2eel7vtbj70hk
+		output := `export DATABASE_USER=USERNAME
+export DATABASE_PASSWORD=PASSWORD
+export DATABASE_SCHEME=postgres
+export DATABASE_HOST=REGION-NAME.compute-1.amazonaws.com
+export DATABASE_PORT=5432
+export DATABASE_DB=d2eel7vtbj70hk
 `
 		testRun(t, input, output)
 	})
 
 	t.Run("MySQL no port", func(t *testing.T) {
 		input := "mysql://USERNAME:PASSWORD@REGIONNAME.cleardb.net/heroku_92599a6734fc1a6?reconnect=true"
-		output := `DATABASE_USER=USERNAME
-DATABASE_PASSWORD=PASSWORD
-DATABASE_SCHEME=mysql
-DATABASE_HOST=REGIONNAME.cleardb.net
-DATABASE_PORT=3306
-DATABASE_DB=heroku_92599a6734fc1a6
+		output := `export DATABASE_USER=USERNAME
+export DATABASE_PASSWORD=PASSWORD
+export DATABASE_SCHEME=mysql
+export DATABASE_HOST=REGIONNAME.cleardb.net
+export DATABASE_PORT=3306
+export DATABASE_DB=heroku_92599a6734fc1a6
 `
 		testRun(t, input, output)
 	})
